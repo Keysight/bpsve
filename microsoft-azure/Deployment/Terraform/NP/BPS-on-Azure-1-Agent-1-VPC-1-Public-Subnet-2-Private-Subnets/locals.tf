501c2f8d725c1a785653b9cf850fba5ab0e6e456
armdupre <a-joseph.dupre@keysight.com>
2024-02-07 13:45:02 -0500

    Initial Checkin.

---
+++ microsoft-azure/Deployment/Terraform/NP/BPS-on-Azure-1-Agent-1-VPC-1-Public-Subnet-2-Private-Subnets/locals.tf
@@ -0,0 +1,15 @@
+locals {
+	AgentVmSize = var.AgentVmSize
+	Agent1InstanceId = "agent1"
+	AppTag = "bps"
+	Preamble = "${local.UserLoginTag}-${local.AppTag}"
+	PublicSecurityRuleSourceIpPrefixes = var.PublicSecurityRuleSourceIpPrefixes
+	ResourceGroupLocation = var.ResourceGroupLocation
+	ResourceGroupName = var.ResourceGroupName
+	SharedImageGalleryName = var.SharedImageGalleryName
+	SharedImageGalleryResourceGroupName = var.SharedImageGalleryResourceGroupName
+	SubscriptionId = var.SubscriptionId
+	UserEmailTag = var.UserEmailTag
+	UserLoginTag = var.UserLoginTag
+	UserProjectTag = var.UserProjectTag
+}
