12404eb3fd276e33eb0175284af6c2c32c10a8e5
armdupre <a-joseph.dupre@keysight.com>
2024-02-07 14:17:38 -0500

    Initial Checkin.

---
+++ microsoft-azure/Deployment/Terraform/BPS/BPS-on-Azure-1-App-1-Agent-1-VPC-1-Public-Subnet-1-Private-Subnet/vnet.tf
@@ -0,0 +1,11 @@
+module "Vnet" {
+	source = "armdupre/module-1-vnet-1-public-subnet-1-private-subnet/azurerm"
+	version = "9.30.2"
+	PublicSecurityRuleSourceIpPrefixes = local.PublicSecurityRuleSourceIpPrefixes
+	ResourceGroupLocation = azurerm_resource_group.ResourceGroup.location
+	ResourceGroupName = azurerm_resource_group.ResourceGroup.name
+	Tag = local.AppTag
+	UserEmailTag = local.UserEmailTag
+	UserLoginTag = local.UserLoginTag
+	UserProjectTag = local.UserProjectTag
+}
