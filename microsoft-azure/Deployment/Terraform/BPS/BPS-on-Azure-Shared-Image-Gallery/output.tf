7b0e4060e3f4a8552876f0044a6a08def1b94d6b
armdupre <a-joseph.dupre@keysight.com>
2024-02-15 22:37:06 -0500

    Updated.

--- microsoft-azure/Deployment/Terraform/BPS/BPS-on-Azure-Shared-Image-Gallery/output.tf
+++ microsoft-azure/Deployment/Terraform/BPS/BPS-on-Azure-Shared-Image-Gallery/output.tf
@@ -1,3 +1,31 @@
+output "AgentSharedImage" {
+	value = {
+		"accelerated_network_support_enabled" : module.SharedImageGallery.AgentSharedImage.accelerated_network_support_enabled
+		"architecture" : module.SharedImageGallery.AgentSharedImage.architecture
+		"gallery_name" : module.SharedImageGallery.AgentSharedImage.gallery_name
+		"hyper_v_generation" : module.SharedImageGallery.AgentSharedImage.hyper_v_generation
+		"location" : module.SharedImageGallery.AgentSharedImage.location
+		"name" : module.SharedImageGallery.AgentSharedImage.name
+		"os_type" : module.SharedImageGallery.AgentSharedImage.os_type
+		"resource_group_name" : module.SharedImageGallery.AgentSharedImage.resource_group_name
+		"identifier" : module.SharedImageGallery.AgentSharedImage.identifier
+    }
+}
+
+output "AppSharedImage" {
+	value = {
+		"accelerated_network_support_enabled" : module.SharedImageGallery.AppSharedImage.accelerated_network_support_enabled
+		"architecture" : module.SharedImageGallery.AppSharedImage.architecture
+		"gallery_name" : module.SharedImageGallery.AppSharedImage.gallery_name
+		"hyper_v_generation" : module.SharedImageGallery.AppSharedImage.hyper_v_generation
+		"location" : module.SharedImageGallery.AppSharedImage.location
+		"name" : module.SharedImageGallery.AppSharedImage.name
+		"os_type" : module.SharedImageGallery.AppSharedImage.os_type
+		"resource_group_name" : module.SharedImageGallery.AppSharedImage.resource_group_name
+		"identifier" : module.SharedImageGallery.AppSharedImage.identifier
+    }
+}
+
 output "SharedImageGallery" {
 	value = {
 		"location" = module.SharedImageGallery.SharedImageGallery.location
