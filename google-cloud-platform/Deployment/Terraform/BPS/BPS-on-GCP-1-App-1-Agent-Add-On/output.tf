214362947c66315dd84875a670375ab4eb8dfe9a
armdupre <a-joseph.dupre@keysight.com>
2024-02-28 13:56:58 -0500

    Initial Checkin.

---
+++ google-cloud-platform/Deployment/Terraform/BPS/BPS-on-GCP-1-App-1-Agent-Add-On/output.tf
@@ -0,0 +1,29 @@
+output "AgentImage" {
+	value = {
+		"family" : module.Agent1.Image.family
+		"name" : module.Agent1.Image.name
+		"project" : module.Agent1.Image.project
+	}
+}
+
+output "Agent1Eth0PublicIpAddress" {
+	value = {
+		"address" : module.Agent1.Eth0PublicIpAddress.address
+		"address_type" : module.Agent1.Eth0PublicIpAddress.address_type
+	}
+}
+
+output "AppEth0PublicIpAddress" {
+	value = {
+		"address" : module.App.Eth0PublicIpAddress.address
+		"address_type" : module.App.Eth0PublicIpAddress.address_type
+	}
+}
+
+output "AppImage" {
+	value = {
+		"family" : module.App.Image.family
+		"name" : module.App.Image.name
+		"project" : module.App.Image.project
+	}
+}
