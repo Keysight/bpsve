439671a1affde522d1d4ab6744346c829bdc2b69
armdupre <a-joseph.dupre@keysight.com>
2024-02-28 13:56:28 -0500

    Initial Checkin.

---
+++ google-cloud-platform/Deployment/Terraform/NP/BPS-on-GCP-1-Agent-Add-On/main.tf
@@ -0,0 +1,17 @@
+module "Agent1" {
+	source = "armdupre/module-bps-agent/google"
+	version = "10.0.1"
+	Eth0SubnetName = data.google_compute_subnetwork.PublicSubnet.name
+	Eth0VpcNetworkName = data.google_compute_network.PublicVpcNetwork.name
+	Eth1SubnetName = data.google_compute_subnetwork.Private1Subnet.name
+	Eth1VpcNetworkName = data.google_compute_network.Private1VpcNetwork.name
+	Eth2SubnetName = data.google_compute_subnetwork.Private2Subnet.name
+	Eth2VpcNetworkName = data.google_compute_network.Private2VpcNetwork.name
+	InstanceId = local.Agent1InstanceId
+	MachineType = local.AgentMachineType
+	RegionName = data.google_client_config.current.region
+	UserEmailTag = local.UserEmailTag
+	UserLoginTag = local.UserLoginTag
+	UserProjectTag = local.UserProjectTag
+	ZoneName = data.google_client_config.current.zone
+}
