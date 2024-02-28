f372a177fd72c0b47a6fa39d00ebe86332f90690
armdupre <a-joseph.dupre@keysight.com>
2024-02-28 13:55:20 -0500

    Initial Checkin.

---
+++ google-cloud-platform/Deployment/Terraform/BPS/BPS-on-GCP-1-App-1-Agent-3-VPCs-1-Public-Subnet-2-Private-Subnets/locals.tf
@@ -0,0 +1,13 @@
+locals {
+	AgentMachineType = var.AgentMachineType
+	Agent1InstanceId = "agent1"
+	AppMachineType = var.AppMachineType
+	AppTag = "bps"
+	Preamble = "${local.UserLoginTag}-${local.UserProjectTag}-${local.AppTag}"
+	Private1VpcNetworkPeerName = "${local.Preamble}-test1-vpc-peer"
+	Private2VpcNetworkPeerName = "${local.Preamble}-test2-vpc-peer"
+	PublicFirewallRuleSourceIpRanges = var.PublicFirewallRuleSourceIpRanges
+	UserEmailTag = var.UserEmailTag
+	UserLoginTag = var.UserLoginTag
+	UserProjectTag = var.UserProjectTag
+}
