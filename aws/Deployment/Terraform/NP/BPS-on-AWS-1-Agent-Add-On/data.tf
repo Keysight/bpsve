8806fe61981efa44b62837145c00718316c7e817
armdupre <a-joseph.dupre@keysight.com>
2025-06-13 13:39:22 -0400

    Updated.

--- aws/Deployment/Terraform/NP/BPS-on-AWS-1-Agent-Add-On/data.tf
+++ aws/Deployment/Terraform/NP/BPS-on-AWS-1-Agent-Add-On/data.tf
@@ -34,6 +34,21 @@ data "aws_subnet" "PublicSubnet" {
     }
 }
 
+data "cloudinit_config" "init_cli" {
+	gzip = true
+	base64_encode = true
+	part {
+		content_type = "text/cloud-config"
+		content = templatefile("cloud-init.yml", {
+			File1Content : local.File1Content
+			File1Name : local.File1Name
+			File2Content : local.File2Content
+			File2Name : local.File2Name
+			UserName: local.AppUserName
+		})
+	}
+}
+
 data "http" "ip" {
 	url = "https://ifconfig.me"
 }
