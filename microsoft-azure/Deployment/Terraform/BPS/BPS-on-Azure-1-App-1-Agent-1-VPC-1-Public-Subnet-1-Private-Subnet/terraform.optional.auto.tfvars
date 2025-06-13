977e77b034a54c91aa542ea3e5f8098f16af26e2
Ovidiu-Dan Francu <ovidiu-dan.francu@keysight.com>
2025-06-13 22:16:50 +0300

    Delete microsoft-azure directory

--- microsoft-azure/Deployment/Terraform/BPS/BPS-on-Azure-1-App-1-Agent-1-VPC-1-Public-Subnet-1-Private-Subnet/terraform.optional.auto.tfvars
+++
@@ -1,5 +0,0 @@
-# AgentVmSize = "Standard_F8s_v2"
-# AppVmSize = "Standard_F4s_v2"
-# ResourceGroupLocation = "East US"
-# SkipProviderRegistration = false
-# UserProjectTag = "cloud-ist"
--- microsoft-azure/Deployment/Terraform/BPS/BPS-on-Azure-Shared-Image-Gallery/locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-	ResourceGroupLocation = var.ResourceGroupLocation
-	ResourceGroupName = var.ResourceGroupName
-	StorageAccountName = var.StorageAccountName
-	UserEmailTag = var.UserEmailTag
-	UserLoginTag = var.UserLoginTag
-	UserProjectTag = var.UserProjectTag
-}
