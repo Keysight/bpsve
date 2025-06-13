e57b011087751ba6b54d5502fddf42ddf40dee9a
armdupre <a-joseph.dupre@keysight.com>
2025-06-13 13:49:26 -0400

    Updated.

--- aws/Deployment/Terraform/BPS/BPS-on-AWS-1-App-1-Agent-1-VPC-1-Public-Subnet-1-Private-Subnet/variables.tf
+++ aws/Deployment/Terraform/BPS/BPS-on-AWS-1-App-1-Agent-1-VPC-1-Public-Subnet-1-Private-Subnet/variables.tf
@@ -46,19 +46,11 @@ variable "ApiMaxRetries" {
 }
 
 variable "AppInstanceType" {
-	default = "t3.xlarge"
+	default = "c5.xlarge"
 	description = "Instance type of App VM"
 	type = string
 	validation {
-		condition = contains([	"t3.xlarge", "t3.2xlarge",
-								"t3a.xlarge", "t3a.2xlarge",
-								"m6i.xlarge", "m6i.2xlarge",
-								"m6a.xlarge", "m6a.2xlarge",
-								"m6in.xlarge", "m6in.2xlarge",
-								"m5.xlarge", "m5.2xlarge",
-								"m5a.xlarge", "m5a.2xlarge",
-								"m5n.xlarge", "m5n.2xlarge",
-								"c6i.xlarge", "c6i.2xlarge",
+		condition = contains([	"c6i.xlarge", "c6i.2xlarge",
 								"c6a.xlarge", "c6a.2xlarge",
 								"c6in.xlarge", "c6in.2xlarge",
 								"c5.xlarge", "c5.2xlarge",
@@ -67,13 +59,6 @@ variable "AppInstanceType" {
 							], var.AppInstanceType)
 		error_message = <<EOF
 AppInstanceType must be one of the following types:
-	t3a.xlarge, t3a.2xlarge,
-	m6i.xlarge, m6i.2xlarge,
-	m6a.xlarge, m6a.2xlarge,
-	m6in.xlarge, m6in.2xlarge,
-	m5.xlarge, m5.2xlarge,
-	m5a.xlarge, m5a.2xlarge,
-	m5n.xlarge, m5n.2xlarge,
 	c6i.xlarge, c6i.2xlarge,
 	c6a.xlarge, c6a.2xlarge,
 	c6in.xlarge, c6in.2xlarge,
