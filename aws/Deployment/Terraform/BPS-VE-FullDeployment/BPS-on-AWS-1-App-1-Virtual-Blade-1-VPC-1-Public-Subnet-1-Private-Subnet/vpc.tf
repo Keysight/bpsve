06738c4801590e19378771e83d3433ac5fb28918
armdupre <a-joseph.dupre@keysight.com>
2025-11-04 11:04:34 -0500

    Updated.

--- aws/Deployment/Terraform/BPS-VE-FullDeployment/BPS-on-AWS-1-App-1-Virtual-Blade-1-VPC-1-Public-Subnet-1-Private-Subnet/vpc.tf
+++ aws/Deployment/Terraform/BPS-VE-FullDeployment/BPS-on-AWS-1-App-1-Virtual-Blade-1-VPC-1-Public-Subnet-1-Private-Subnet/vpc.tf
@@ -1,5 +1,5 @@
 module "Vpc" {
-	source = "git::https://github.com/armdupre/terraform-aws-module-1-vpc-1-public-subnet-1-private-subnet.git?ref=11.0.0"
+	source = "git::https://github.com/armdupre/terraform-aws-module-1-vpc-1-public-subnet-1-private-subnet.git?ref=11.20.0"
 	InboundIPv4CidrBlocks = local.InboundIPv4CidrBlocks
 	PrivateSubnetAvailabilityZone = local.PrivateSubnetAvailabilityZone
 	PublicSubnetAvailabilityZone = local.PublicSubnetAvailabilityZone
