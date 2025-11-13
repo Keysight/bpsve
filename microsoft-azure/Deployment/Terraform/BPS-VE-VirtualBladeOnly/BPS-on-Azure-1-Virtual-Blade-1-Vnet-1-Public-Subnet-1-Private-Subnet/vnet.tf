96d7f7f91ef6c91418c337713743ab37a71dcc22
armdupre <a-joseph.dupre@keysight.com>
2025-11-13 07:26:28 -0500

    Updated.

---
+++ microsoft-azure/Deployment/Terraform/BPS-VE-VirtualBladeOnly/BPS-on-Azure-1-Virtual-Blade-1-Vnet-1-Public-Subnet-1-Private-Subnet/vnet.tf
@@ -0,0 +1,10 @@
+module "Vnet" {
+	source = "git::https://github.com/armdupre/terraform-azurerm-module-1-vnet-1-public-subnet-1-private-subnet.git?ref=11.20.0"
+	PublicSecurityRuleSourceIpPrefixes = local.PublicSecurityRuleSourceIpPrefixes
+	ResourceGroupLocation = azurerm_resource_group.ResourceGroup.location
+	ResourceGroupName = azurerm_resource_group.ResourceGroup.name
+	Tag = local.AppTag
+	UserEmailTag = local.UserEmailTag
+	UserLoginTag = local.UserLoginTag
+	UserProjectTag = local.UserProjectTag
+}
