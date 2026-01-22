8d252b460862ee77e122dc7c07e9068ea3c79e1d
armdupre <a-joseph.dupre@keysight.com>
2026-01-22 10:30:36 -0500

    Updated.

--- aws/Deployment/Terraform/BPS-VE-VirtualBladeOnly/BPS-on-AWS-1-Virtual-Blade-1-VPC-1-Public-Subnet-1-Private-Subnet/main.tf
+++ aws/Deployment/Terraform/BPS-VE-VirtualBladeOnly/BPS-on-AWS-1-Virtual-Blade-1-VPC-1-Public-Subnet-1-Private-Subnet/main.tf
@@ -1,5 +1,5 @@
 module "Agent1" {
-	source = "git::https://github.com/armdupre/terraform-aws-module-bps-agent.git?ref=11.20.0"
+	source = "git::https://github.com/Keysight/terraform-aws-module-bps-agent.git?ref=26.0.0"
 	Eth0SecurityGroupId = module.Vpc.PublicSecurityGroup.id
 	Eth0SubnetId = module.Vpc.PublicSubnet.id
 	Eth1SecurityGroupId = module.Vpc.PrivateSecurityGroup.id
@@ -7,14 +7,22 @@ module "Agent1" {
 	Eth2PrivateIpAddresses = local.Agent1Eth2PrivateIpAddresses
 	Eth2SecurityGroupId = module.Vpc.PrivateSecurityGroup.id
 	Eth2SubnetId = module.Vpc.PrivateSubnet.id
+	InstanceId = local.Agent1InstanceId
 	InstanceType = local.AgentInstanceType
+	PlacementGroupId = aws_placement_group.PlacementGroup.id
 	UserEmailTag = local.UserEmailTag
 	UserLoginTag = local.UserLoginTag
 	UserProjectTag = local.UserProjectTag
 	init_cli = data.cloudinit_config.init_cli.rendered
 	depends_on = [
+		aws_placement_group.PlacementGroup,
 		module.Vpc
 	]
+}
+
+resource "aws_placement_group" "PlacementGroup" {
+	name = local.PlacementGroupName
+	strategy = local.PlacementGroupStrategy
 }
 
 resource "random_id" "RandomId" {
