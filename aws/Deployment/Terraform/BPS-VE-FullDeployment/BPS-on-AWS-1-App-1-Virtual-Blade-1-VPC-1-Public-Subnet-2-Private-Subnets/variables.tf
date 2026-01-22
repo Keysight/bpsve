8069ad4b76ed14b573c0ee342be0cab9b982a173
armdupre <a-joseph.dupre@keysight.com>
2026-01-22 10:36:33 -0500

    Updated.

--- aws/Deployment/Terraform/BPS-VE-FullDeployment/BPS-on-AWS-1-App-1-Virtual-Blade-1-VPC-1-Public-Subnet-2-Private-Subnets/variables.tf
+++ aws/Deployment/Terraform/BPS-VE-FullDeployment/BPS-on-AWS-1-App-1-Virtual-Blade-1-VPC-1-Public-Subnet-2-Private-Subnets/variables.tf
@@ -1,41 +1,14 @@
 variable "AgentInstanceType" {
-	default = "c5n.xlarge"
+	default = "c6in.xlarge"
 	description = "Instance type of Agent VM"
 	type = string
 	validation {
-		condition = contains([	"t3.xlarge", "t3.2xlarge",
-								"t3a.xlarge", "t3a.2xlarge",
-								"m6i.xlarge", "m6i.2xlarge", "m6i.4xlarge", "m6i.8xlarge", "m6i.12xlarge", "m6i.16xlarge", "m6i.24xlarge", "m6i.32xlarge",
-								"m6a.xlarge", "m6a.2xlarge", "m6a.4xlarge", "m6a.8xlarge", "m6a.12xlarge", "m6a.16xlarge", "m6a.24xlarge", "m6a.32xlarge", "m6a.48xlarge",
-								"m6in.xlarge", "m6in.2xlarge", "m6in.4xlarge", "m6in.8xlarge", "m6in.12xlarge", "m6in.16xlarge", "m6in.24xlarge", "m6in.32xlarge",
-								"m5.xlarge", "m5.2xlarge", "m5.4xlarge", "m5.8xlarge", "m5.12xlarge", "m5.16xlarge", "m5.24xlarge",
-								"m5a.xlarge", "m5a.2xlarge", "m5a.4xlarge", "m5a.8xlarge", "m5a.12xlarge", "m5a.16xlarge", "m5a.24xlarge",
-								"m5n.xlarge", "m5n.2xlarge", "m5n.4xlarge", "m5n.8xlarge", "m5n.12xlarge", "m5n.16xlarge", "m5n.24xlarge",
-								"c6i.xlarge", "c6i.2xlarge", "c6i.4xlarge", "c6i.8xlarge", "c6i.12xlarge", "c6i.16xlarge", "c6i.24xlarge", "c6i.32xlarge",
-								"c6a.xlarge", "c6a.2xlarge", "c6a.4xlarge", "c6a.8xlarge", "c6a.12xlarge", "c6a.16xlarge", "c6a.24xlarge", "c6a.32xlarge", "c6a.48xlarge",
-								"c6in.xlarge", "c6in.2xlarge", "c6in.4xlarge", "c6in.8xlarge", "c6in.12xlarge", "c6in.16xlarge", "c6in.24xlarge", "c6in.32xlarge",
-								"c5.xlarge", "c5.2xlarge", "c5.4xlarge", "c5.9xlarge", "c5.12xlarge", "c5.18xlarge", "c5.24xlarge",
-								"c5a.xlarge", "c5a.2xlarge", "c5a.4xlarge", "c5a.8xlarge", "c5a.12xlarge", "c5a.16xlarge", "c5a.24xlarge",
-								"c5n.xlarge", "c5n.2xlarge", "c5n.4xlarge", "c5n.9xlarge", "c5n.18xlarge",
-								"c4.xlarge", "c4.2xlarge", "c4.4xlarge", "c4.8xlarge"
-							], var.AgentInstanceType)
+		condition = contains([ "c5n.xlarge", "c5n.2xlarge", "c5n.4xlarge", "c5n.9xlarge", "c5n.18xlarge",
+							   "c6in.xlarge", "c6in.2xlarge", "c6in.4xlarge", "c6in.8xlarge", "c6in.12xlarge", "c6in.16xlarge", "c6in.24xlarge", "c6in.32xlarge" ], var.AgentInstanceType)
 		error_message = <<EOF
 AgentInstanceType must be one of the following types:
-	t3.xlarge, t3.2xlarge,
-	t3a.xlarge, t3a.2xlarge,
-	m6i.xlarge, m6i.2xlarge, m6i.4xlarge, m6i.8xlarge, m6i.12xlarge,  m6i.16xlarge, m6i.24xlarge, m6i.32xlarge,
-	m6a.xlarge, m6a.2xlarge, m6a.4xlarge, m6a.8xlarge, m6a.12xlarge, m6a.16xlarge, m6a.24xlarge, m6a.32xlarge, m6a.48xlarge,
-	m6in.xlarge, m6in.2xlarge, m6in.4xlarge, m6in.8xlarge, m6in.12xlarge, m6in.16xlarge, m6in.24xlarge, m6in.32xlarge,
-	m5.xlarge, m5.2xlarge, m5.4xlarge, m5.8xlarge, m5.12xlarge, m5.16xlarge, m5.24xlarge
-	m5a.xlarge, m5a.2xlarge, m5a.4xlarge, m5a.8xlarge, m5a.12xlarge, m5a.16xlarge, m5a.24xlarge,
-	m5n.xlarge, m5n.2xlarge, m5n.4xlarge, m5n.8xlarge, m5n.12xlarge, m5n.16xlarge, m5n.24xlarge,
-	c6i.xlarge, c6i.2xlarge, c6i.4xlarge, c6i.8xlarge, c6i.12xlarge, c6i.16xlarge, c6i.24xlarge, c6i.32xlarge,
-	c6a.xlarge, c6a.2xlarge, c6a.4xlarge, c6a.8xlarge, c6a.12xlarge, c6a.16xlarge, c6a.24xlarge, c6a.32xlarge, c6a.48xlarge,
-	c6in.xlarge, c6in.2xlarge, c6in.4xlarge, c6in.8xlarge, c6in.12xlarge, c6in.16xlarge, c6in.24xlarge, c6in.32xlarge,
-	c5.xlarge, c5.2xlarge, c5.4xlarge, c5.9xlarge, c5.12xlarge, c5.18xlarge, c5.24xlarge,
-	c5a.xlarge, c5a.2xlarge, c5a.4xlarge, c5a.8xlarge, c5a.12xlarge, c5a.16xlarge, c5a.24xlarge,
 	c5n.xlarge, c5n.2xlarge, c5n.4xlarge, c5n.9xlarge, c5n.18xlarge,
-	c4.xlarge, c4.2xlarge, c4.4xlarge, c4.8xlarge
+	c6in.xlarge, c6in.2xlarge, c6in.4xlarge, c6in.8xlarge, c6in.12xlarge, c6in.16xlarge, c6in.24xlarge, c6in.32xlarge
 		EOF
 	}
 }
@@ -50,21 +23,10 @@ variable "AppInstanceType" {
 	description = "Instance type of App VM"
 	type = string
 	validation {
-		condition = contains([	"c6i.xlarge", "c6i.2xlarge",
-								"c6a.xlarge", "c6a.2xlarge",
-								"c6in.xlarge", "c6in.2xlarge",
-								"c5.xlarge", "c5.2xlarge",
-								"c5a.xlarge", "c5a.2xlarge",
-								"c5n.xlarge", "c5n.2xlarge"
-							], var.AppInstanceType)
+		condition = contains([ "c5.xlarge", "c5.2xlarge" ], var.AppInstanceType)
 		error_message = <<EOF
 AppInstanceType must be one of the following types:
-	c6i.xlarge, c6i.2xlarge,
-	c6a.xlarge, c6a.2xlarge,
-	c6in.xlarge, c6in.2xlarge,
-	c5.xlarge, c5.2xlarge,
-	c5a.xlarge, c5a.2xlarge,
-	c5n.xlarge, c5n.2xlarge
+	c5.xlarge, c5.2xlarge
 		EOF
 	}
 }
