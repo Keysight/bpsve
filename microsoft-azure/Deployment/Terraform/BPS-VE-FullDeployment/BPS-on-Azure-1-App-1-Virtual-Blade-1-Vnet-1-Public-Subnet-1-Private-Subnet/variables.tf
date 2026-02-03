7fa955dc209c4feae7c2c95a9605b768988d98c2
armdupre <a-joseph.dupre@keysight.com>
2026-02-03 13:53:06 -0500

    Updated.

--- microsoft-azure/Deployment/Terraform/BPS-VE-FullDeployment/BPS-on-Azure-1-App-1-Virtual-Blade-1-Vnet-1-Public-Subnet-1-Private-Subnet/variables.tf
+++ microsoft-azure/Deployment/Terraform/BPS-VE-FullDeployment/BPS-on-Azure-1-App-1-Virtual-Blade-1-Vnet-1-Public-Subnet-1-Private-Subnet/variables.tf
@@ -3,11 +3,10 @@ variable "AgentVmSize" {
 	description = "Category, series and instance specifications associated with the Agent VM"
 	type = string
 	validation {
-		condition = contains([	"Standard_E8_v5",	"Standard_E16_v5"
-							], var.AgentVmSize)
+		condition = contains([ "Standard_E8_v5", "Standard_E16_v5" ], var.AgentVmSize)
 		error_message = <<EOF
 AgentVmSize must be one of the following sizes:
-	Standard_F4s_v2, Standard_E8_v5, Standard_E16_v5
+	Standard_E8_v5, Standard_E16_v5
 		EOF
 	}
 }
