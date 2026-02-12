32f7aaaf12fe9a06bc8da067acc3062c23198887
armdupre <a-joseph.dupre@keysight.com>
2026-02-12 08:51:56 -0500

    Updated.

--- google-cloud-platform/Deployment/Terraform/BPS-VE-VirtualBladeOnly/BPS-on-GCP-1-Virtual-Blade-3-VPCs-1-Public-Subnet-2-Private-Subnets/main.tf
+++ google-cloud-platform/Deployment/Terraform/BPS-VE-VirtualBladeOnly/BPS-on-GCP-1-Virtual-Blade-3-VPCs-1-Public-Subnet-2-Private-Subnets/main.tf
@@ -1,5 +1,5 @@
 module "Agent1" {
-	source = "git::https://github.com/armdupre/terraform-google-module-bps-agent.git?ref=11.20.3"
+	source = "git::https://github.com/Keysight/terraform-google-module-bps-agent.git?ref=26.0.0"
 	Eth0SubnetName = module.Vpc.PublicSubnet.name
 	Eth0VpcNetworkName = module.Vpc.PublicVpcNetwork.name
 	Eth1SubnetName = module.Vpc.Private1Subnet.name
