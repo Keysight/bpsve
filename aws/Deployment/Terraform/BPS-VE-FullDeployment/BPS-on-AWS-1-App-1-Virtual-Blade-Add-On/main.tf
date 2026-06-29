e15b7e8f61662352823b7e749f93225038fce16c
armdupre <a-joseph.dupre@keysight.com>
2026-06-29 11:02:23 -0400

    Update main.tf, main.tf, main.tf, and 3 more files

--- aws/Deployment/Terraform/BPS-VE-FullDeployment/BPS-on-AWS-1-App-1-Virtual-Blade-Add-On/main.tf
+++ aws/Deployment/Terraform/BPS-VE-FullDeployment/BPS-on-AWS-1-App-1-Virtual-Blade-Add-On/main.tf
@@ -1,5 +1,5 @@
 module "App" {
-	source = "git::https://github.com/Keysight/terraform-aws-module-bps-app.git?ref=26.0.0"
+	source = "git::https://github.com/Keysight/terraform-aws-module-bps-app.git?ref=26.1.0"
 	Eth0SecurityGroupId = data.aws_security_group.PublicSecurityGroup.id
 	Eth0SubnetId = data.aws_subnet.PublicSubnet.id
 	InstanceType = local.AppInstanceType
@@ -10,7 +10,7 @@ module "App" {
 }
 
 module "Agent1" {
-	source = "git::https://github.com/Keysight/terraform-aws-module-bps-agent.git?ref=26.0.0"
+	source = "git::https://github.com/Keysight/terraform-aws-module-bps-agent.git?ref=26.1.0"
 	Eth0SecurityGroupId = data.aws_security_group.PublicSecurityGroup.id
 	Eth0SubnetId = data.aws_subnet.PublicSubnet.id
 	Eth1SecurityGroupId = data.aws_security_group.PrivateSecurityGroup.id
