21fc925c83ec7abca5ef184cb3636e6cf03cc9e6
armdupre <a-joseph.dupre@keysight.com>
2026-07-04 23:24:44 -0400

    Update main.tf, main.tf, main.tf, and 1 more file

--- google-cloud-platform/Deployment/Terraform/BPS-VE-VirtualBladeOnly/BPS-on-GCP-1-Virtual-Blade-3-VPCs-1-Public-Subnet-2-Private-Subnets/main.tf
+++ google-cloud-platform/Deployment/Terraform/BPS-VE-VirtualBladeOnly/BPS-on-GCP-1-Virtual-Blade-3-VPCs-1-Public-Subnet-2-Private-Subnets/main.tf
@@ -1,5 +1,5 @@
 module "Agent1" {
-	source = "git::https://github.com/Keysight/terraform-google-module-bps-agent.git?ref=26.0.0"
+	source = "git::https://github.com/Keysight/terraform-google-module-bps-agent.git?ref=26.1.0"
 	Eth0SubnetName = module.Vpc.PublicSubnet.name
 	Eth0VpcNetworkName = module.Vpc.PublicVpcNetwork.name
 	Eth1SubnetName = module.Vpc.Private1Subnet.name
