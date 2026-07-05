b3fcaf6008aa825975cd407c31f30eba1173ecd9
armdupre <a-joseph.dupre@keysight.com>
2026-07-04 21:06:22 -0400

    Update vnet.tf, main.tf, vnet.tf, and 3 more files

--- microsoft-azure/Deployment/Terraform/BPS-VE-VirtualBladeOnly/BPS-on-Azure-1-Virtual-Blade-Add-On/locals.tf
+++ microsoft-azure/Deployment/Terraform/BPS-VE-VirtualBladeOnly/BPS-on-Azure-1-Virtual-Blade-Add-On/locals.tf
@@ -1,5 +1,8 @@
 locals {
 	AgentVmSize = var.AgentVmSize
+	Agent1Eth0IpAddress = "10.0.10.12"
+	Agent1Eth1IpAddresses = [ "10.0.2.22", "10.0.2.23", "10.0.2.24", "10.0.2.25", "10.0.2.26", "10.0.2.27", "10.0.2.28", "10.0.2.29", "10.0.2.30", "10.0.2.31" ]
+	Agent1Eth2IpAddresses = [ "10.0.3.22", "10.0.3.23", "10.0.3.24", "10.0.3.25", "10.0.3.26", "10.0.3.27", "10.0.3.28", "10.0.3.29", "10.0.3.30", "10.0.3.31" ]
 	Agent1InstanceId = "agent1"
 	AppTag = "bps"
 	AppUserName = "ixia"
